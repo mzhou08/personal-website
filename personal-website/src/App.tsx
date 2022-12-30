0b6b4cfd8a24755645246f499ccf3c02baf8c061
Michael Zhou <mhzhou@andrew.cmu.edu>
2022-12-29 23:12:06 -0500

    switched project to TypeScript

---
+++ personal-website/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from "./components/common/NavBar/NavBar";
+
+
+function App() {
+  return (
+    <div className="App">
+        <NavBar />
+    </div>
+  );
+}
+
+export default App;
