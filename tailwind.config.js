5f8b4d7e2ae1433b16aa64e6664dee916179d85e
Michael Zhou <mhzhou@andrew.cmu.edu>
2023-06-03 12:16:29 -0700

    added font family and padding customs

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'header': ["Century\\ Gothic", "CenturyGothic", "AppleGothic", "sans-serif"],
+      'body': ["Helvetica\\ Neue", "Helvetica", "Arial", "sans-serif"],
+    },
+    extend: {
+      padding: {
+        '1/2': '50%',
+        full: '100%',
+      },
+      height: {
+        '1/10': '10%',
+      },
+    },
   },
   plugins: [],
 }
