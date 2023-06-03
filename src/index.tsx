74b94bcf9d669fbbeab587547d523edcfa55afa4
Michael Zhou <mhzhou@andrew.cmu.edu>
2023-06-03 16:43:26 -0700

    removed Chakra UI

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import "./index.css";
-
-import theme from './static/chakra-styles/theme';
 
 const rootElement: HTMLElement  = document.getElementById('root')!;
 
@@ -15,11 +12,9 @@ const root = ReactDOM.createRoot(rootElement);
 
 
 root.render(
-  <ChakraProvider theme={theme}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ChakraProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
