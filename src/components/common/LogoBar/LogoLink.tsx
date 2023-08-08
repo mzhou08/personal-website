83e583b1df6e7cfb498fe76ae9d090240ddfd9b6
Michael Zhou <mhzhou@andrew.cmu.edu>
2023-08-08 13:27:00 -0700

    removed shadow, lowered opacity

--- src/components/common/LogoBar/LogoLink.tsx
+++ src/components/common/LogoBar/LogoLink.tsx
@@ -6,10 +6,10 @@ function LogoLink(
     props: { icon: IconProp; link: string; }
 ) {
     return (
-        <a href={props.link} className="drop-shadow-md h-full pr-2 pl-2">
+        <a href={props.link} className="h-full pr-2 pl-2">
             <FontAwesomeIcon
                 icon={props.icon}
-                className="h-full text-pastel-400"
+                className="h-full text-pastel-text/50"
             />
         </a>
     )
