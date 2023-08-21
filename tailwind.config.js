70b02ee32a9d8d90b9ebc7537225a86d3fe7e7c0
Michael Zhou <mhzhou@andrew.cmu.edu>
2023-08-21 16:03:35 -0400

    animation styles and color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     fontFamily: {
       'header': ["Century\\ Gothic", "CenturyGothic", "AppleGothic", "sans-serif"],
-      'body': ["Helvetica\\ Neue", "Helvetica", "Arial", "sans-serif"],
+      'body': ["Optima", "Segoe", "Segoe UI", "Candara", "Calibri", "Arial", "sans-serif"],
     },
     extend: {
       inset: {
@@ -22,10 +22,25 @@ module.exports = {
       },
       colors: {
         'pastel-100': '#FCF8ED',
-        'pastel-200': '#D5DDD4',
-        'pastel-300': '#825E6F',
-        'pastel-400': '#32414A',
+        'pastel-200': '#DCDED5',
+        'pastel-300': '#B899C7',
+        'pastel-400': '#5B74D7',
         'pastel-500': '#181925',
+        'pastel-text': '#181925',
+      },
+      keyframes: {
+        rolodex: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '25%, 50%': { transform: 'translateY(-25%)' },
+          '50%, 75%': { transform: 'translateY(-50%)' },
+          '75%': { transform: 'translateY(-75%)' },
+        }
+      },
+      transitionTimingFunction: {
+        'expo': 'cubic-bezier(.75,0,.25,1)',
+      },
+      animation: {
+        rolodex: 'rolodex 16s ease-expo infinite',
       },
     },
   },
