aee257769da696ee1f56e7e05dfdb5263c2abaf4
Michael Zhou <mhzhou@andrew.cmu.edu>
2023-09-02 16:11:03 -0400

    removed import

--- src/components/pages/About/About.tsx
+++ src/components/pages/About/About.tsx
@@ -1,5 +1,3 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import React from 'react';
 import BuildPage from '../../common/BuildPage';
 import HorizontalBar from '../../common/HorizontalBar/HorizontalBar';
