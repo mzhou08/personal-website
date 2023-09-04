c79fa6f3548a10f79bdcba35b3ce5ed8cb316494
Michael Zhou <mhzhou@andrew.cmu.edu>
2023-09-04 19:31:54 -0400

    updated Home page

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,37 +1,46 @@
 import React from 'react';
 import BuildPage from '../../common/BuildPage';
 import HorizontalBar from '../../common/HorizontalBar/HorizontalBar';
+import { Link } from 'react-router-dom';
 
 
 function HomeComponent() {
     return (
         <div className="w-full">
-            <h1 className="font-header text-left text-pastel-text \
-            text-2xl font-bold tracking-widest \
-            pt-4">
-                Welcome
+            <h1 className="text-center">
+                WELCOME
             </h1>
-            <div className="font-header text-left text-pastel-text \
-            text-normal font-regular tracking-widest \
-            pt-4">
-                My corner of the Internet.
-            </div>
+
+            <h2 className="text-center">
+                Michael Zhou's Game of Life player card
+            </h2>
 
             <HorizontalBar center={false}/>
 
-            <div className="">
-                I made this site with React.js and TailwindCSS. Source code can be
-                found on <a
+            <ul className="fa-ul list-none list-inside">
+                <li><i className="fa-li fa-solid fa-address-card opacity-50 scale-75"></i>
+                    <Link to="/about">My (very summarized) life story and interests</Link>
+                </li>
+                <li><i className="fa-li fa-solid fa-wrench opacity-50 scale-75"></i>
+                    <Link to="/projects">Cool things I've built</Link>
+                </li>
+                <li><i className="fa-li fa-solid fa-briefcase opacity-50 scale-75"></i>
+                    <Link to="/work">Teaching & professional experience</Link>
+                </li>
+                <li><i className="fa-li fa-solid fa-file opacity-50 scale-75"></i>
+                    <Link to="/resume">Downloadable resum&#233;</Link>
+                </li>
+                <li><i className="fa-li fa-solid fa-address-book opacity-50 scale-75"></i>
+                    <Link to="/resume">My contact info (Let's talk!)</Link>
+                </li>
+                <li><i className="fa-li fa-solid fa-code opacity-50 scale-75"></i> Made with React + Tailwind. Feel free to use <a
                         href="https://github.com/mzhou08/personal-website"
                         className="underline"
                     >
-                        Github
-                    </a>. Feel free to use & reuse as you please!
-                <br/><br/>
-                On this site, you can find everything from my resume to projects to recent reads (coming soon). Enjoy!
-                <br/><br/>
-                Disclaimer: This site is still a work in progress, so there may be a few quirks.
-            </div>
+                        as a template
+                    </a>!
+                </li>
+            </ul>
         </div>
         
     )
