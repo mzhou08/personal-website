3cacfcea23c1109bf4ec3db64ad99868af08e719
Michael Zhou <mhzhou@andrew.cmu.edu>
2023-09-15 00:43:39 -0400

    fixed scroll pos not resetting

--- src/components/common/BuildPage.tsx
+++ src/components/common/BuildPage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import NavBar from './NavBar/NavBar';
 import SideBar from './SideBar/SideBar';
 import GameOfLife from './GameOfLife/GameOfLife';
+import ScrollResetOnMount from './ScrollReset';
 
 export default function BuildPage(
     props: any,
@@ -9,6 +10,7 @@ export default function BuildPage(
 ) {
     return (
         <div className="h-full w-full bg-pastel-100">
+            <ScrollResetOnMount />
             <div className="absolute w-3/4 right-0 top-1/10">
                 <div className="w-4/5 m-auto bg-pastel-200 p-16">
                     { displayComponent(props) /* ADD MAX WIDTH HERE */}
